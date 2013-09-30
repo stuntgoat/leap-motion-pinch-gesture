96531e263915394b76c380e956f8e43ff6a2cb10
Isaac Gonzalez <isaac@overttone.com>
2013-09-30 01:23:57 -0700

    factor out circbuf; more comments; return location of pinch

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"runtime"
 	"time"
 
-	"github.com/stuntgoat/leap-motion-pinch-gesture/circbuf"
+	"github.com/stuntgoat/circbuf"
 	"github.com/whoisjake/gomotion"
 )
 
@@ -17,7 +17,8 @@ const (
 	// number of consecutive values before timeout
 	PINCH_DETECT_THRESHOLD = 40
 
-	PINCH_DISTANCE_THRESHOLD = 1800
+	// threshold between 2 pointables to register a pinch
+	PINCH_DISTANCE_THRESHOLD = 1900
 
 	// number of pointables to store in a circular buffer
 	// for each finger per hand id.
@@ -25,11 +26,13 @@ const (
 
 	// number of times that the last distance between 2 points
 	// can be greater that the current point when checking convergence
-	CONVERGENCE_THRESHOLD = 4
+	CONVERGENCE_THRESHOLD = 6
 
 	DEBUG = true
 	DEBUG_COLLECT = true
-	DEBUG_CONVERGENCE = false
+	DEBUG_DISTANCE = true
+	DEBUG_CONVERGENCE = true
+	DEBUG_PINCH = true
 )
 
 var logger *log.Logger
@@ -38,18 +41,6 @@ func Debug(msg string) {
 	if DEBUG {
 		logger.Println(msg)
 	}
-}
-
-type Point struct {
-	X float64
-	Y float64
-	Z float64
-	HandId int
-}
-
-func (p *Point) SetFromPointable(pointable *gomotion.Pointable) {
-	p.X, p.Y, p.Z = float64(pointable.TipPosition[0]), float64(pointable.TipPosition[1]), float64(pointable.TipPosition[2])
-	p.HandId = pointable.HandId
 }
 
 
@@ -62,12 +53,9 @@ type MyPointable struct {
 // calculateConvergence takes 2 MyPointable objects and calculates
 // the difference between the last 15 points between each of the
 // 2 points given.
-func calculateConvergence(p1, p2 *MyPointable) bool {
+func (mp *MyPointable) CalculateConvergence(p *MyPointable) bool {
 	// get last point data to confirm if the points converging.
-	var pointA = Point{}
 	var myA interface{}
-
-	var pointB = Point{}
 	var myB interface{}
 
 	var dCurrent float64
@@ -75,25 +63,35 @@ func calculateConvergence(p1, p2 *MyPointable) bool {
 
 	var failThreshold int
 
-	// we check every other value from the history
-	for i := 0; i < MAX_POINTABLES_PER_HISTORY; i += 2 {
-		// may be jank
-		myA, _ = p1.History.ReadFromStart(i)
+	var p1Len = mp.History.Added
+	var p2Len = p.History.Added
 
-		// 2 assert interface is implements Pointable
-		myAp, ok := myA.(gomotion.Pointable)
-		if ok {
-			pointA.SetFromPointable(&myAp)
+	var max int64
+	var maxCount int
+
+	if p1Len == p2Len || p1Len < p2Len {
+		maxCount = int(p1Len)
+	} else {
+		maxCount = int(p2Len)
+	}
+
+	if max > MAX_POINTABLES_PER_HISTORY {
+		maxCount = MAX_POINTABLES_PER_HISTORY
+	}
+
+	for i := 0; i < maxCount; i += 2 { // skip every other frame
+		myA, _ = mp.History.ReadFromStart(i)
+		myAp, okA := myA.(gomotion.Pointable)
+
+		myB, _ = p.History.ReadFromStart(i)
+		myBp, okB := myB.(gomotion.Pointable)
+
+		if okA && okB {
+			dCurrent = DistanceBetweenPointables(&myAp, &myBp)
+		} else {
+			logger.Fatal("fail to cast interface from circular buffer to a Pointable")
 		}
 
-		myB, _ = p2.History.ReadFromStart(i)
-
-		myBp, ok := myB.(gomotion.Pointable)
-		if ok {
-			pointA.SetFromPointable(&myBp)
-		}
-
-		dCurrent = DistBtwnPoints(&pointA, &pointB)
 		if dLast < dCurrent && dLast != 0 {
 			failThreshold++
 		}
@@ -107,7 +105,6 @@ func calculateConvergence(p1, p2 *MyPointable) bool {
 		return false
 	}
 	return true
-
 }
 
 
@@ -124,7 +121,7 @@ type HandPinchCheck struct {
 	PointableChan chan gomotion.Pointable
 
 	// sends a pinch event to the listener
-	PinchChan chan *Pinch
+	PinchChan *chan *Pinch
 
 	// pointable id to last update time
 	LastUpdate map[int]time.Time
@@ -137,11 +134,12 @@ type HandPinchCheck struct {
 }
 
 func (hPinchCheck *HandPinchCheck) ListenForPointables() {
-	// pointable id to MyPointable object
+
 	var pair []*MyPointable
 	var myP *MyPointable
 	var ok bool
 	var converging bool
+	var args []*gomotion.Pointable
 
 	for {
 		select {
@@ -161,21 +159,16 @@ func (hPinchCheck *HandPinchCheck) ListenForPointables() {
 			myP.lastUpdate = time.Now()
 
 		case <- hPinchCheck.FingerDisappeared:
-			// check if we have only 2 pointables that have been seen
-			// very recently
-			// check the number of pointables that are less than 5ms old
-			// if only 2, and distance between is less than X, send event
-			pair = make([]*MyPointable, 0)
+			// Check if we have only 2 pointables that have been seen
+			// recently.
 
+			pair = make([]*MyPointable, 0)
 			for _, pntbl := range hPinchCheck.Pointables {
+				// Check the number of pointables that are new
 				isNew := bool(time.Since(pntbl.lastUpdate) < time.Duration(50 * time.Millisecond))
-				// we check it the last update time is recent and
-				// if there are at least 8 frames in history
-				if isNew && (pntbl.History.Added >= 8) {
+				// at least a few frames in history?
+				if isNew && (pntbl.History.Added >= 6) {
 					pair = append(pair, pntbl)
-					if DEBUG_COLLECT == false {
-						continue
-					}
 				}
 				if DEBUG_COLLECT {
 					msg := fmt.Sprintf("pointable is new: %t", isNew)
@@ -184,47 +177,55 @@ func (hPinchCheck *HandPinchCheck) ListenForPointables() {
 					Debug(msg)
 				}
 			}
+
+			// two fingers might mean a pinch occured
 			if len(pair) == 2 {
+				// extract the Pointable from the circular buffer
 				var lastItem interface{}
 				var err error
-				args := make([]Point, 0)
+				args = make([]*gomotion.Pointable, 0)
 
 				for _, pntbl := range pair {
-					point := Point{}
 
-					// last item
 					lastItem, err = pntbl.History.ReadFromEnd(0)
 					if err != nil {
 						logger.Fatal(err.Error)
 					}
 					goP, ok := lastItem.(gomotion.Pointable)
 					if ok {
-						point.SetFromPointable(&goP)
-						args = append(args, point)
+						args = append(args, &goP)
 					}
 				}
-				dist := hPinchCheck.distBetweenPoints(args[0], args[1])
-				if dist < PINCH_DISTANCE_THRESHOLD {
-					converging = calculateConvergence(pair[0], pair[1])
 
-					// TODO: - send coordinate of pinch
+				dist := DistanceBetweenPointables(args[0], args[1])
+				if dist < PINCH_DISTANCE_THRESHOLD {
+
+					converging = pair[0].CalculateConvergence(pair[1])
 					if converging {
-						Debug("PINCH DETECTED")
+						fmt.Println("sending pinch on pinch channel")
+
+						pinch := new(Pinch)
+						pinch.SetFrom2Pointables(args[0], args[1])
+						*hPinchCheck.PinchChan <- pinch
 						goto REMOVE_OLD
 					}
-					Debug("failed to converge")
-				} else  {
-					msg := fmt.Sprintf("PINCH_DISTANCE_THRESHOLD: %d\tdistance: %f", PINCH_DISTANCE_THRESHOLD, dist)
-					Debug(msg)
+					if DEBUG_CONVERGENCE {
+						Debug("failed to converge")
+					}
+
+				} else {
+					if DEBUG_DISTANCE {
+						logger.Printf("PINCH_DISTANCE_THRESHOLD: %d\tdistance: %f\n", PINCH_DISTANCE_THRESHOLD, dist)
+					}
 				}
 			}
-			if DEBUG {
+			if DEBUG_COLLECT {
 				logger.Printf("could not find 2 pointables. Found %d", len(pair))
 			}
 			goto REMOVE_OLD
 		}
+
 	REMOVE_OLD:
-		// remove old pointables
 		for id, mpt := range hPinchCheck.Pointables {
 			if time.Since(mpt.lastUpdate) > time.Duration(60 * time.Millisecond) {
 				delete(hPinchCheck.Pointables, id)
@@ -233,60 +234,45 @@ func (hPinchCheck *HandPinchCheck) ListenForPointables() {
 	}
 }
 
-func (hPinchCheck *HandPinchCheck) distBetweenPoints(a, b Point) float64 {
-	return DistanceBetween(a.X, a.Y, a.Z, b.X, b.Y, b.Z)
-}
-
-func DistBtwnPoints(p1, p2 *Point) float64 {
-	return DistanceBetween(p1.X, p1.Y, p1.Z, p2.X, p2.Y, p2.Z)
-}
 
 
-// var HandOnePinch = make(chan gomotion.Pointable, PINCH_DETECT_THRESHOLD * 2)
-// var HandTwoPinch = make(chan gomotion.Pointable, PINCH_DETECT_THRESHOLD * 2)
-
-// send all pointables to this router and
+// HandPinchCheck reads LeapMotion frames sends Pinch objects shortly
+// after they occur.
 type HandPinchRouter struct {
+	// listens for frames
 	FrameChan chan *gomotion.Frame
-	PinchChecks map[int]HandPinchCheck // map of hand ids to
-	// TODO: get the pinch events out
+
+	// map of hand ids to objects that calculate Pinch events for that hand.
+	PinchChecks map[int]HandPinchCheck
+
+	// emits Pinch object pointers
+	PinchChan chan *Pinch
 }
 
-
+// PPH stands for Pointables Per Hand; used for counting pointables
+// per hand within the HandPinchRouter
 type PPH struct {
 	HandId int
 	NumPointables int
-	lastUpdate time.Time
-}
-
-func clear() {
-	fmt.Println("\033[2J\033[H")
 }
 
 // RouteHands will route a Pointable to the respective hand pinch
 // channel.
 func (hPinchRouter *HandPinchRouter) RouteHand() {
-	// TODO: remove old values in maps to prevent memory leak
-
-	// check pointables per hand
 	var currPerHand = map[int]*PPH{}
 	var pastPerHand = map[int]*PPH{}
 	var handId int
 	for frame := range hPinchRouter.FrameChan {
-		// need to keep track of the number of fingers that are here
-		// and the number that were before this to register
-		// when a pinch event occurs on a hand
-
-		// for debugging
-		// clear()
-
 		for _, pointable := range frame.Pointables {
 			handId = pointable.HandId
+
+			// sometimes we receive a frame with an unknown hand id
 			if handId == -1 {
 				continue
 			}
 
-			// check the current hand id pointables
+			// check the current hand id pointables; 'pointables per hand'.
+			// If one is not found, we create one
 			if pph, ok := currPerHand[handId]; ok {
 				pph.NumPointables += 1
 			} else {
@@ -308,6 +294,7 @@ func (hPinchRouter *HandPinchRouter) RouteHand() {
 					LastUpdate: make(map[int]time.Time),
 					Pointables: make(map[int]*MyPointable),
 					FingerDisappeared: make(chan bool),
+					PinchChan: &hPinchRouter.PinchChan,
 				}
 				go hpc.ListenForPointables()
 				hpc.PointableChan <- pointable
@@ -318,15 +305,9 @@ func (hPinchRouter *HandPinchRouter) RouteHand() {
 		// if so, send the FingerDisappeared to the hand's FingerDisappeared chan.
 		for pHandId, pph := range pastPerHand {
 			if cHnum, ok := currPerHand[pHandId]; ok {
-				// race condition!!!
 				if cHnum.NumPointables < pph.NumPointables {
-					// pHandId had 1 or more fingers disappear:
-					// FingerDisappeared!!!
 					hPinchRouter.PinchChecks[pHandId].FingerDisappeared <- true;
 				}
-			} else {
-				// pHandId had all fingers disappear: FingerDisappeared!!!
-				hPinchRouter.PinchChecks[pHandId].FingerDisappeared <- true;
 			}
 		}
 		pastPerHand = currPerHand
@@ -334,11 +315,41 @@ func (hPinchRouter *HandPinchRouter) RouteHand() {
 	}
 }
 
+// Pinch represents a pinch event in 3 dimensional space for a particular hand.
 type Pinch struct {
-	Point
-	HandID int
+	X float64
+	Y float64
+	Z float64
+	HandId int
 }
 
+// SetFrom2Pointables sets the fields in a Pinch object from 2 gomotion.Pointables.
+func (p *Pinch) SetFrom2Pointables(p1, p2 *gomotion.Pointable) {
+	p1X, p1Y, p1Z := XYZFromPointable(p1)
+	p2X, p2Y, p2Z := XYZFromPointable(p2)
+
+	p.X = Halfway(p1X, p2X)
+	p.Y = Halfway(p1Y, p2Y)
+	p.Z = Halfway(p1Z, p2Z)
+	p.HandId = p1.HandId
+}
+
+
+func XYZFromPointable(p *gomotion.Pointable) (x, y, z float64) {
+	return float64(p.TipPosition[0]), float64(p.TipPosition[1]), float64(p.TipPosition[2])
+}
+
+func DistanceBetweenPointables(pointable1, pointable2 *gomotion.Pointable) float64 {
+	p1X, p1Y, p1Z := XYZFromPointable(pointable1)
+	p2X, p2Y, p2Z := XYZFromPointable(pointable2)
+
+	return DistanceBetween(p1X, p1Y, p1Z, p2X, p2Y, p2Z)
+}
+
+// Halfway returns the coordinate between 2 coordinates in the same dimension.
+func Halfway(a, b float64) float64 {
+	return float64(a + (0.5 * (b - a)))
+}
 
 // DistanceBetween takes the distance between 2 3D points and
 // caluclates the total distance between them by calculating the
@@ -361,20 +372,31 @@ func main() {
 
 	device, err := gomotion.GetDevice("ws://127.0.0.1:6437/v3.json")
 	if err != nil {
-		panic(err.Error())
+		logger.Fatal(err.Error())
 	}
     defer device.Close()
+
     device.Listen()
 
 	var router = HandPinchRouter{
 		FrameChan: make(chan *gomotion.Frame),
 		PinchChecks: make(map[int]HandPinchCheck),
+		PinchChan: make(chan *Pinch),
 	}
 
 	go router.RouteHand()
-	// send frames to frames channel in a goroutine
+	for {
+		select {
+		case frame := <- device.Pipe:
+			router.FrameChan <- frame
 
-	for frame := range device.Pipe {
-		router.FrameChan <- frame
+		case pinch := <- router.PinchChan:
+			// your code goes here :)
+
+
+			if DEBUG_PINCH {
+				logger.Printf("PINCH DETECTED: %+v", pinch)
+			}
+		}
 	}
 }
